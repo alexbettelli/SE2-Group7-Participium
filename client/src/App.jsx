a08f54e69d11e0bc7c13e2a03719afb40c8926a5
aurora gennaro s339054 <s339054@studenti.polito.it>
2025-11-05 10:04:03 +0100

    added overview component, page, and temporary home

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,17 @@
-import './App.css'
+import { Routes, Route } from 'react-router-dom';
+import { HomePage } from './pages/TempHome.jsx';
+import { ReportOverviewPage } from './pages/ReportOverviewPage.jsx';
+import './App.css';
 
 function App() {
-
   return (
-    <>
-    </>
-  )
+    <div className="App">
+      <Routes>
+        <Route index element={<HomePage />} />
+        <Route path="/report-overview" element={<ReportOverviewPage />} />
+      </Routes>
+    </div>
+  );
 }
 
-export default App
+export default App;
