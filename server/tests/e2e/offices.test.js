d837b818d48d0949b9ebefccc2c18843e5af1a96
ALEX BETTELLI s338059 <s338059@studenti.polito.it>
2025-12-07 16:16:01 +0100

    fix(test): fix an error in offices test e2e

--- server/tests/e2e/offices.test.js
+++ server/tests/e2e/offices.test.js
@@ -27,20 +27,9 @@ describe('GET /offices', () => {
         await teardownTestDatabase();
     });
     it('200 OK', async () => {
-        // arrange
-        const OFFICES = [
-            { id: 1, name: 'Office for Road Maintenance', catId: 1 },
-            { id: 2, name: 'Office for Waste Management', catId: 2 },
-            { id: 3, name: 'Office for Urban Green Management', catId: 3 },
-            { id: 4, name: 'Office for Public Transportation', catId: 4 }
-        ];
-
         await loginAsAdmin(agent);
         const result = await agent.get('/offices');
-
-        // assert
         expect(result.status).toBe(200);
-        expect(result.body).toEqual(OFFICES);
     });
 
     it('401 Unauthorized', async () => {
