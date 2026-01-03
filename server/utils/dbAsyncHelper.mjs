763b833854c80d80890e011af0ae81ffb32bbb0c
ALEX BETTELLI s338059 <s338059@studenti.polito.it>
2026-01-03 17:01:25 +0100

    feat(SonarQube): fix ReportDao issues

---
+++ server/utils/dbAsyncHelper.mjs
@@ -0,0 +1,17 @@
+import db from "../data/db.mjs";
+
+export const runAsync = (sql, params = []) =>
+  new Promise((resolve, reject) => {
+    db.run(sql, params, function (err) {
+      if (err) return reject(err);
+      resolve(this);
+    });
+  });
+
+export const getAsync = (sql, params = []) =>
+  new Promise((resolve, reject) => {
+    db.get(sql, params, (err, row) => {
+      if (err) return reject(err);
+      resolve(row);
+    });
+  });
